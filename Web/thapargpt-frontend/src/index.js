0371c623e08897f90534253f7d8c166a594bf439
Aryan-Kamboj11 <kambojaryankamboj@gmail.com>
2025-05-13 21:48:59 +0530

    Made changes in index.js

--- Web/thapargpt-frontend/src/index.js
+++ Web/thapargpt-frontend/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 ReactDOM.render(
   <BrowserRouter basename="/ThaparENV/WEB/thapargpt-frontend">
